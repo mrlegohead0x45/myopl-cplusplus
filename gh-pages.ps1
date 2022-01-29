a6bc1dba82d0afd8cf1f57da8d0657f9ff6941f0
mrlegohead0x45 <96256923+mrlegohead0x45@users.noreply.github.com>
2022-01-29 16:26:12 +0000

    Update gh-pages.ps1

--- gh-pages.ps1
+++ gh-pages.ps1
@@ -1,10 +1,23 @@
+# Rebuild docs
 Set-Location .\docs
 .\make.bat html
 Set-Location ..
+
+# Switch to gh-pages
+# docs\build\html will still be there because it is untracked
 git checkout gh-pages
-Remove-Item .\_sources .\_static
+
+# Remove these so that we can move the new ones to the same paths
+Remove-Item .\_sources -Recurse
+Remove-Item .\_static -Recurse
+
+# Move everything to the root directory
 Move-Item .\docs\build\html\* -Force .
+
+# Delete the docs directory
 Remove-Item -Recurse .\docs
+
+# Add, commit and return to main
 git add .
 git commit -m "Rebuild documentation"
 git checkout main
